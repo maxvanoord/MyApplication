98ec20c85a760aa74cdd39b44e35b40e09560a3b
Max van Oord <max.van.oord@gmail.com>
2019-03-06 23:23:24 +0100

    xxx

--- app/src/main/java/com/example/myapplication/ProductScreen.java
+++ app/src/main/java/com/example/myapplication/ProductScreen.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ProductScreen extends Activity{
 
     ImageView drone;
+    Button lenen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,21 @@ public class ProductScreen extends Activity{
         setContentView(R.layout.productscreen);
 
         drone = findViewById(R.id.Drone);
+        lenen = findViewById(R.id.leenbutton);
 
         drone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent droneIntent = new Intent(ProductScreen.this, DroneScreen.class);
                 startActivity(droneIntent);
+            }
+        });
+
+        lenen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lenenIntent = new Intent(ProductScreen.this, LeenActivity.class);
+                startActivity(lenenIntent);
             }
         });
 
