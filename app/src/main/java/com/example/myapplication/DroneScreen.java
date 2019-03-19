b06e4f6d3c22af17a0419e1c1857e3ff6fa5a9a0
Joeri <joerihuigsloot@icloud.com>
2019-03-19 21:08:18 +0100

    add shopping_cart @ dronescreen

--- app/src/main/java/com/example/myapplication/DroneScreen.java
+++ app/src/main/java/com/example/myapplication/DroneScreen.java
@@ -1,13 +1,28 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DroneScreen extends Activity{
+
+    ImageView shopping_cart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dronescreen);
+
+        shopping_cart = findViewById(R.id.shoppingcart);
+
+        shopping_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shoppingcartIntent = new Intent(DroneScreen.this, BestellingGeslaagd.class);
+                startActivity(shoppingcartIntent);
+            }
+        });
     }
 }
