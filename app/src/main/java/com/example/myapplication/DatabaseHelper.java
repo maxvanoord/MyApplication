7ab7b1764146ea6e308a7527823ea0a7e3186a6d
Max van Oord <max.van.oord@gmail.com>
2019-05-21 18:43:24 +0200

    Revert "Merge branch 'joeri' of C:\Users\Gebruiker\Documents\GitHub\MyApplication\MyApplication with conflicts."
    This reverts commit 233929008ab4f3f5bce3e30e415c982825ac0939.

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -16,6 +16,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_EMAIL = "email";
     private static final String COLUMN_PASS = "pass";
     SQLiteDatabase db;
+
     private static final String TABLE_CREATE = "create table contacts (id integer primary key not null , " +
             "name text not null , email text not null , pass text not null);";
 
@@ -23,7 +24,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(Context context)
     {
         super(context , DATABASE_NAME ,null , DATABASE_VERSION);
-
     }
 
     @Override
@@ -32,45 +32,62 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.db = db;
     }
 
-    public void insertContact(Contact_Database c) {
-        db = this.getWritableDatabase();
+    public void insertContact(Contact_Database c) {     // Functie om een contact toe te voegen in de database
+        db = this.getWritableDatabase();                // Openen van de database
         ContentValues values = new ContentValues();
+
 
         String query =  "select * from contacts";
         Cursor cursor = db.rawQuery(query, null);
-        int count = cursor.getCount();
+        int count = cursor.getCount();                  // Count wordt gebruikt om te tellen welk id de colum moet krijgen
 
-        values.put(COLUMN_ID, count);
+
+        values.put(COLUMN_ID, count);                   // values wordt een een tuple met (COLUMN_ID = count, ...)
         values.put(COLUMN_NAME , c.getUsername());
         values.put(COLUMN_EMAIL , c.getEmail());
         values.put(COLUMN_PASS , c.getPassword());
 
-        db.insert(TABLE_NAME,null,values);
+        db.insert(TABLE_NAME,null,values);  // van tuple values wordt een object aan de table name gevoegd
         db.close();
     }
 
-    public String searchPass(String username)
-    {
-        db = this.getReadableDatabase();
-        String query = "select username, pass from "+TABLE_NAME;
-        Cursor cursor = db.rawQuery(query, null);
-        String a, b;
-        b = "not found";
-        if(cursor.moveToFirst())
-        {
-            do{
-                a = cursor.getString(0);
 
-                if(a.equals(username))
-                {
-                    b = cursor.getString(1);
-                    break;
-                }
-            }
-            while(cursor.moveToNext());
-        }
-        return null;
+
+
+
+    public Boolean checkMail(String given_mail){
+        db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("Select * from contacts where email=?", new String[]{given_mail});
+        if (cursor.getCount()>0) return false;
+        else return true;
     }
+
+    public Boolean checkUsername(String given_username){
+        db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("Select * from contacts where name=?", new String[]{given_username});
+        if (cursor.getCount()>0) return false;
+        else return true;
+    }
+
+
+
+    public Boolean checkMatch(String username1, String password){
+        db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("select * from contacts where name=? and pass=?", new String[]{username1, password});
+        if (cursor.getCount()>0) return true;
+        else return false;
+    }
+
+    public void eraseTable(){           // deze functie kan worden opgeroepen om een table te legen
+        db = this.getReadableDatabase();
+        db.execSQL("delete from contacts");
+    }
+
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
