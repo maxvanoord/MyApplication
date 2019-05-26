61723de51712de5256905eea5384c26c5b6dddd7
Joeri <joerihuigsloot@icloud.com>
2019-05-26 16:02:22 +0200

    spinner toegevoegd met caterogien

--- app/src/main/java/com/example/myapplication/AdminAddProducts.java
+++ app/src/main/java/com/example/myapplication/AdminAddProducts.java
@@ -3,10 +3,14 @@ package com.example.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class AdminAddProducts extends Activity {
+public class AdminAddProducts extends Activity implements OnItemSelectedListener {
 
     DatabaseHelper helper = new DatabaseHelper(this);
 
@@ -41,6 +45,22 @@ public class AdminAddProducts extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adminaddproducts);
 
+        Spinner spinner = findViewById(R.id.choose_categorie);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
