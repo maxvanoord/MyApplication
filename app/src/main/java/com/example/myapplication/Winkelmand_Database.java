d6a33d25d5a790783b3e0f438f45c35e7450833d
rverwayen <47591601+rverwayen@users.noreply.github.com>
2019-06-05 11:48:19 +0200

    winkelmand database

---
+++ app/src/main/java/com/example/myapplication/Winkelmand_Database.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+public class Winkelmand_Database {
+    String name;
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
